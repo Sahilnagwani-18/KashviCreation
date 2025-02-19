d31c7787b29db9d983f6372ff0a6dea2228a4bbd
Sahil <sahilnagwani121@gmail.com>
2025-02-19 21:14:07 +0530

    Push

--- Backend/server.js
+++ Backend/server.js
@@ -29,7 +29,7 @@ console.log("Allowed Origin:", process.env.FRONTEND_URL);
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017";  
 mongoose
-  .connect(`${MONGODB_URI}/Kashvi5`)
+  .connect(`${MONGODB_URI}`)
   .then(() => console.log("✅ Database Connected Successfully"))
   .catch((err) => console.error("❌ Database Connection Error:", err));
 
